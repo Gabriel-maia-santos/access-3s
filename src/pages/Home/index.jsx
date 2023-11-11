85f3078d9c070ef5f17e67d812c8c4290434265a
Gabriel Maia dos Santos <61596749+Gabriel-maia-santos@users.noreply.github.com>
2023-11-11 12:44:23 -0300

    70% feito home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { Button, Flex, Segmented } from 'antd';
+import { Flex, Space, Table, Tag } from 'antd';
+import { Button, ButtonGroup } from "@nextui-org/react";
 
 const justifyOptions = [
     'flex-start',
@@ -13,18 +14,95 @@ const justifyOptions = [
 const alignOptions = ['flex-start', 'center', 'flex-end'];
 
 
+const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+        render: (text) => <a>{text}</a>,
+    },
+    {
+        title: 'Age',
+        dataIndex: 'age',
+        key: 'age',
+    },
+    {
+        title: 'Address',
+        dataIndex: 'address',
+        key: 'address',
+    },
+    {
+        title: 'Tags',
+        key: 'tags',
+        dataIndex: 'tags',
+        render: (_, { tags }) => (
+            <>
+                {tags.map((tag) => {
+                    let color = tag.length > 5 ? 'geekblue' : 'green';
+                    if (tag === 'loser') {
+                        color = 'volcano';
+                    }
+                    return (
+                        <Tag color={color} key={tag}>
+                            {tag.toUpperCase()}
+                        </Tag>
+                    );
+                })}
+            </>
+        ),
+    },
+    {
+        title: 'Action',
+        key: 'action',
+        render: (_, record) => (
+            <Space size="middle">
+                <a>Invite {record.name}</a>
+                <a>Delete</a>
+            </Space>
+        ),
+    },
+];
+const data = [
+    {
+        key: '1',
+        name: 'John Brown',
+        age: 32,
+        address: 'New York No. 1 Lake Park',
+        tags: ['nice', 'developer'],
+    },
+    {
+        key: '2',
+        name: 'Jim Green',
+        age: 42,
+        address: 'London No. 1 Lake Park',
+        tags: ['loser'],
+    },
+    {
+        key: '3',
+        name: 'Joe Black',
+        age: 32,
+        address: 'Sydney No. 1 Lake Park',
+        tags: ['cool', 'teacher'],
+    },
+];
+
+
 export const Home = () => {
 
         const [justify, setJustify] = React.useState(justifyOptions[3]);
         const [alignItems, setAlignItems] = React.useState(alignOptions[1]);
     return (
             <body>
-                <Flex justify={justify} align={alignItems}>
-                    <Button type="primary">Primary</Button>
-                    <Button type="primary">Primary</Button>
-                    <Button type="primary">Primary</Button>
-                    <Button type="primary">Primary</Button>
+            <Flex style={{ backgroundColor: "#006FEE" }} justify={justify} align={alignItems}>
+                    <Button type="primary"></Button>
+                <div style={{ margin: 10 }}>
+                    <Button style={{ margin: 10 }} type="primary">Ponto Digital</Button>
+                    <Button style={{ margin: 10 }} type="primary">Cadastro de usuario</Button>
+                </div>
+
             </Flex>
+            <Table columns={columns} dataSource={data} />
+
         </body>
 
         );
