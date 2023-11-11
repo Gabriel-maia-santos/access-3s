1ff988f09327e325493f47b10343ef922ad3436c
Gabriel Maia dos Santos <61596749+Gabriel-maia-santos@users.noreply.github.com>
2023-11-11 12:08:07 -0300

    configuracao de ambiente

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+        <Router>
+            <Switch>
+                <Route path="/login" component={Login} />
+                <Route path="/cadastro" component={Cadastro} />
+                <Route path="/home" component={Home} />
+            </Switch>
+        </Router>
   </React.StrictMode>,
 )
