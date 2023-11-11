17aa92fe60d852d4dac75938054044a00bb6dfef
Gabriel Maia dos Santos <61596749+Gabriel-maia-santos@users.noreply.github.com>
2023-11-11 12:34:20 -0300

    arrumando css

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
+import { Button, Flex, Segmented } from 'antd';
+
+const justifyOptions = [
+    'flex-start',
+    'center',
+    'flex-end',
+    'space-between',
+    'space-around',
+    'space-evenly',
+];
+const alignOptions = ['flex-start', 'center', 'flex-end'];
+
+
 export const Home = () => {
-    return (<>
-        Home
-    </>)
+
+        const [justify, setJustify] = React.useState(justifyOptions[3]);
+        const [alignItems, setAlignItems] = React.useState(alignOptions[1]);
+    return (
+            <body>
+                <Flex justify={justify} align={alignItems}>
+                    <Button type="primary">Primary</Button>
+                    <Button type="primary">Primary</Button>
+                    <Button type="primary">Primary</Button>
+                    <Button type="primary">Primary</Button>
+            </Flex>
+        </body>
+
+        );
 }
